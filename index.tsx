b0314e9a5bd7a66e58d4f4ced0e82cdf155c5cec
Elmer Galo <elmer.galo@outlook.com>
2023-04-15 20:36:08 -0400

    add development server with mock data

--- index.tsx
+++ index.tsx
@@ -1,7 +1,12 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
+import { makeServer } from "@/server";
 
 import App from "./src/App";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 
 const Main = () => {
   return <App />;
---
+++ server/index.ts
@@ -0,0 +1,53 @@
+import { createServer, Response } from "miragejs";
+import data from "./mock-data";
+
+export function makeServer({ environment = "test" } = {}) {
+  console.log("🚀 ~ Development Server running...");
+
+  let server = createServer({
+    environment,
+
+    seeds(server) {
+      server.db.loadData(data);
+    },
+
+    routes() {
+      this.namespace = "api";
+      this.timing = 1500;
+
+      this.get("/get-news-data", (schema) => {
+        return { data: schema.db.data };
+      });
+
+      this.post("/reset-password", () => {
+        throw new Error("Reset password not available on Dev mode");
+      });
+
+      this.post("/login", () => {
+        return new Response(
+          500,
+          { "Content-Type": "application/json" },
+          { general: "Login not available on Dev Mode" }
+        );
+      });
+
+      this.post("/signup", () => {
+        return new Response(
+          500,
+          { "Content-Type": "application/json" },
+          { general: "Signup not available on Dev Mode" }
+        );
+      });
+
+      this.post("/update-user-password", () => {
+        return new Response(
+          500,
+          { "Content-Type": "application/json" },
+          "Reset Password not available on Dev Mode"
+        );
+      });
+    },
+  });
+
+  return server;
+}
