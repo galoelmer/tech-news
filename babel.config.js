5c1707925d2695b3917fff473caefcee46ff076f
Elmer Galo <elmer.galo@outlook.com>
2023-04-15 20:35:04 -0400

    chore: resolve root module

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = function (api) {
         "module-resolver",
         {
           alias: {
+            ["@"]: "./",
             components: "./src/components",
             screens: "./src/screens",
             assets: "./assets",
