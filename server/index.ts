41e8dc94d1b0ab09bdeba744ff20259029aba856
Elmer Galo <elmer.galo@outlook.com>
2023-05-04 18:59:08 -0400

    add http request logs to mock server

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,13 @@
 import { createServer, Response } from "miragejs";
+import { nanoid } from "@reduxjs/toolkit";
+import clc from "cli-color";
+
 import data from "./mock-data";
 
+const token = nanoid();
+
 export function makeServer({ environment = "test" } = {}) {
-  console.log("🚀 ~ Development Server running...");
+  console.log(clc.bgMagentaBright("🚀 ~ Development Server running..."));
 
   let server = createServer({
     environment,
@@ -15,6 +20,8 @@ export function makeServer({ environment = "test" } = {}) {
       this.namespace = "api";
       this.timing = 1500;
 
+      this.pretender.handledRequest = (_, __, request) => logger(request);
+
       this.get("/get-news-data", (schema) => {
         return { data: schema.db.data };
       });
@@ -23,7 +30,17 @@ export function makeServer({ environment = "test" } = {}) {
         throw new Error("Reset password not available on Dev mode");
       });
 
-      this.post("/login", () => {
+      this.post("/login", (_, request) => {
+        const { email, password } = JSON.parse(request.requestBody);
+
+        if (email === "test@email.com" && password === "123Pass") {
+          return new Response(
+            200,
+            { "Content-Type": "application/json" },
+            { token }
+          );
+        }
+
         return new Response(
           500,
           { "Content-Type": "application/json" },
@@ -51,3 +68,31 @@ export function makeServer({ environment = "test" } = {}) {
 
   return server;
 }
+
+type Request = Parameters<
+  ReturnType<typeof createServer>["pretender"]["handledRequest"]
+>[2] & { url?: string; method?: string };
+
+const logger = (request: Request) => {
+  const { method, url, requestBody, queryParams, requestHeaders, status } =
+    request;
+
+  if (method && url && status) {
+    console.log(clc.blueBright(`[${method}] ${status} ${url}`));
+  }
+
+  if (Object.keys(queryParams).length > 0) {
+    console.log(clc.yellowBright(`params: ${JSON.stringify(queryParams)}`));
+  }
+
+  if (Object.keys(requestHeaders).length > 0) {
+    console.log(
+      clc.magentaBright(`headers: ${JSON.stringify(requestHeaders)}`)
+    );
+  }
+
+  if (requestBody) {
+    const body = JSON.stringify(JSON.parse(requestBody));
+    console.log(clc.greenBright(`body: ${body}`));
+  }
+};
