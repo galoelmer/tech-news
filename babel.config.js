b021f5abdcf085ab4ca0acac265ecbfb7d7423c0
Elmer Galo <elmer.galo@outlook.com>
2023-05-15 19:16:10 -0400

    add prettier and eslint

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,36 @@
+/* eslint-disable */
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
-        "module-resolver",
+        'module-resolver',
         {
           alias: {
-            "@": "./src",
-            components: "./src/components",
-            screens: "./src/screens",
-            hooks: "./src/hooks",
-            assets: "./assets",
-            context: "./src/context",
-            services: "./src/services",
-            utils: "./src/utils",
-            navigation: "./src/navigation",
-          },
-        },
+            '@': './src',
+            components: './src/components',
+            screens: './src/screens',
+            hooks: './src/hooks',
+            assets: './assets',
+            context: './src/context',
+            services: './src/services',
+            utils: './src/utils',
+            navigation: './src/navigation'
+          }
+        }
       ],
       [
-        "react-native-reanimated/plugin",
+        'react-native-reanimated/plugin',
         {
-          relativeSourceLocation: true,
-        },
-      ],
+          relativeSourceLocation: true
+        }
+      ]
     ],
     env: {
       production: {
-        plugins: ["react-native-paper/babel"],
-      },
-    },
+        plugins: ['react-native-paper/babel']
+      }
+    }
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,8 @@
-const { getDefaultConfig } = require("expo/metro-config");
+/* eslint-disable */
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.assetExts.push("web.ts", "web.tsx");
+config.resolver.assetExts.push('web.ts', 'web.tsx');
 
 module.exports = config;
